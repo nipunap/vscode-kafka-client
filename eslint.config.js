1b8b126343fed014a2ec9ed74ea337ad3a5c7163
Nipuna Perera <nipunap@gmail.com>
2025-10-05 08:53:21 +0100

    fix: migrate to ESLint 9 flat config format

---
+++ eslint.config.js
@@ -0,0 +1,45 @@
+const tseslint = require('typescript-eslint');
+const eslint = require('@eslint/js');
+
+module.exports = tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    languageOptions: {
+      parserOptions: {
+        project: './tsconfig.json',
+        ecmaVersion: 2022,
+        sourceType: 'module',
+      },
+    },
+    rules: {
+      '@typescript-eslint/naming-convention': [
+        'warn',
+        {
+          selector: 'import',
+          format: ['camelCase', 'PascalCase'],
+        },
+      ],
+      '@typescript-eslint/semi': 'warn',
+      'curly': 'warn',
+      'eqeqeq': 'warn',
+      'no-throw-literal': 'warn',
+      'semi': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-require-imports': 'off',
+    },
+  },
+  {
+    ignores: [
+      'out/**',
+      'dist/**',
+      '**/*.d.ts',
+      'node_modules/**',
+      '.vscode-test/**',
+      'mics/**',
+      'scripts/**/*.js',
+    ],
+  }
+);
+
